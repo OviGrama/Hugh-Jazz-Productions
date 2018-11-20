ce672a60fd158c4f64ca7a124420e3e033fce0bd
OviGrama <44976044+OviGrama@users.noreply.github.com>
2018-11-20 11:29:42 +0000

    Random Name Generator
    Two arrays for first and last names lists, it will pick at random a first and a last name at run time.

---
+++ Studio Prototypes/Assets/Scripts/OG_RandomNameGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OG_RandomNameGenerator : MonoBehaviour {
+
+    public string[] FirstNamesList;
+    public string[] LastNamesList;
+    public string FirstName;
+    public string LastName;
+
+    public TextMesh FirstNameText;
+    public TextMesh LastNameText;
+    // Use this for initialization
+    void Start () {
+        FirstName = FirstNamesList[Random.Range(0, FirstNamesList.Length)];
+        LastName = LastNamesList[Random.Range(0, LastNamesList.Length)];
+        FirstNameText.text = FirstName.ToString();
+        LastNameText.text = LastName.ToString();
+
+    }
+}
