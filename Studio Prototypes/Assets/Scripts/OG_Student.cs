c1873b117f15fad37a91a002f52f2b60354ab39d
OviGrama <44976044+OviGrama@users.noreply.github.com>
2018-11-26 17:37:07 +0000

    26.11.2018
    Class go to tests

---
+++ Studio Prototypes/Assets/Scripts/OG_Student.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OG_Student : MonoBehaviour {
+
+    //public Transform Target_Dest;
+    float fl_speed = 5;
+    public GameObject go_UIpanel;
+    public float fl_DistanceToDest;
+    public GameObject[] Classes;
+    public int i;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Classes[i].GetComponent<OG_Classes>().bl_Unlocked)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Classes[i].transform.position, fl_speed * Time.deltaTime);
+        }
+
+       
+		
+	}
+}
