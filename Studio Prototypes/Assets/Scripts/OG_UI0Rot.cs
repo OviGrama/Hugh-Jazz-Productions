49a06a27c1a62b57cabd7f942a34c71e3864a739
OviGrama <44976044+OviGrama@users.noreply.github.com>
2018-12-02 23:21:18 +0000

    Calendar D&D  &  HoverStats

---
+++ Studio Prototypes/Assets/Scripts/OG_UI0Rot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OG_UI0Rot : MonoBehaviour
+{
+
+    private Quaternion rotation;
+
+    void Awake()
+    {
+        rotation = transform.rotation;
+    }
+
+    void LateUpdate()
+    {
+        transform.rotation = rotation;
+    }
+}
