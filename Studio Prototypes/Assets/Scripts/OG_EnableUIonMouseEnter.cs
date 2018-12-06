db499cb7882763ff7806cec67e9038454eeb9f00
OviGrama <44976044+OviGrama@users.noreply.github.com>
2018-12-06 18:20:32 +0000

    OG_UI Menues_Students

---
+++ Studio Prototypes/Assets/Scripts/OG_EnableUIonMouseEnter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OG_EnableUIonMouseEnter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    public float fl_fadetime;
+    public Canvas canvas;
+    public bool bl_displayStats;
+
+    // Use this for initialization
+    void Start()
+    {
+        canvas = GameObject.Find("StudentInfo").GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DisplayStats();
+
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!bl_displayStats)
+        { 
+            bl_displayStats = true;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if(bl_displayStats)
+        {
+            bl_displayStats = false;
+        }
+    }
+
+
+
+    //private void OnMouseUp()
+    //{
+    //    bl_displayStats = false;
+    //}
+
+    void DisplayStats()
+    {
+        if (bl_displayStats)
+        {
+            canvas.enabled = true;
+        }
+        else
+        {
+            canvas.enabled = false;
+        }
+    }
+}
