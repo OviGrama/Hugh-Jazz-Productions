e0515d489b2d3ad1f85755a824adea4ab21c5ab4
jhutchines <44975942+jhutchines@users.noreply.github.com>
2018-12-10 16:00:38 +0000

    Time-Update
    - Adds day cycle
    - Time of day changes current lesson

---
+++ Studio Prototypes/Assets/Scripts/JH_Time_UI.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JH_Time_UI : MonoBehaviour {
+
+    public enum ListDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+
+    public ListDays currentDay;
+
+    public float fl_timeSpeed;
+    private int in_currentTime = 8;
+    private bool bl_progressTime = true;
+    private bool bl_changeClass = true;
+    private GameObject go_classManager;
+    private Text tx_days;
+    private Text tx_time;
+
+	// Use this for initialization
+	void Start () {
+        go_classManager = GameObject.Find("Main Camera");
+        tx_days = transform.GetChild(0).GetComponent<Text>();
+        tx_time = transform.GetChild(1).GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (bl_progressTime) StartCoroutine(ProgressTime());
+        ChangeDay();
+        ChangeClass();
+	}
+
+    IEnumerator ProgressTime()
+    {
+        bl_progressTime = false;
+        yield return new WaitForSeconds(fl_timeSpeed);
+        in_currentTime++;
+        if (in_currentTime == 18) in_currentTime = 7;
+        bl_progressTime = true;
+    }
+
+    void ChangeDay()
+    {
+        if (go_classManager.GetComponent<JH_Class_Manager>().currentClass == 0)
+        {
+            currentDay = ListDays.Monday;
+            tx_days.text = currentDay.ToString();
+        }
+        if (go_classManager.GetComponent<JH_Class_Manager>().currentClass == 2)
+        {
+            currentDay = ListDays.Tuesday;
+            tx_days.text = currentDay.ToString();
+        }
+        if (go_classManager.GetComponent<JH_Class_Manager>().currentClass == 4)
+        {
+            currentDay = ListDays.Wednesday;
+            tx_days.text = currentDay.ToString();
+        }
+        if (go_classManager.GetComponent<JH_Class_Manager>().currentClass == 6)
+        {
+            currentDay = ListDays.Thursday;
+            tx_days.text = currentDay.ToString();
+        }
+        if (go_classManager.GetComponent<JH_Class_Manager>().currentClass == 8)
+        {
+            currentDay = ListDays.Friday;
+            tx_days.text = currentDay.ToString();
+        }
+
+        tx_time.text = in_currentTime.ToString() + ":00";
+    }
+
+    void ChangeClass()
+    {
+        if (bl_changeClass && in_currentTime == 12)
+        {
+            go_classManager.GetComponent<JH_Class_Manager>().currentClass++;
+            bl_changeClass = false;
+        }
+
+        if (in_currentTime == 13) bl_changeClass = true;
+
+        if (bl_changeClass && in_currentTime == 7)
+        {
+            go_classManager.GetComponent<JH_Class_Manager>().currentClass++;
+            bl_changeClass = false;
+        }
+
+        if (in_currentTime == 8) bl_changeClass = true;
+    }
+}
