127203e7acbbe553464d379b87704cc472f4dcff
jhutchines <44975942+jhutchines@users.noreply.github.com>
2019-01-28 17:24:03 +0000

    Upgrade Systems
    Yes

---
+++ Studio Prototypes/Assets/Scripts/JH_Upgrade_Room.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JH_Upgrade_Room : MonoBehaviour
+{
+    public GameObject upgradePanel;
+    private bool upgradingRoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (upgradingRoom) ClickToUpgrade();
+    }
+
+    public void UpgradeRoom()
+    {
+        if (upgradePanel.activeInHierarchy)
+        {
+            upgradePanel.SetActive(false);
+            upgradingRoom = false;
+        }
+        else
+        {
+            upgradePanel.SetActive(true);
+            upgradingRoom = true;
+        }
+    }
+
+    void ClickToUpgrade()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit.collider.gameObject.GetComponent<JH_Class_Main>() != null)
+                {
+                    hit.collider.gameObject.GetComponent<JH_Class_Main>().UpgradeRoom();
+                    UpgradeRoom();
+                }
+                else UpgradeRoom();
+            }
+        }
+    }
+}
