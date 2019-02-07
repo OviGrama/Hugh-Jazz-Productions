c536f085d3afd9cd2b4231614ffa672ecda89f9b
jhutchines <44975942+jhutchines@users.noreply.github.com>
2019-02-07 16:58:35 +0000

    JH_Upgrade_Upgraded
    - Added green outline when hovering over upgradable room
    - Added padlock sprite over the currently locked classrooms
    - Changed the time UI colours to match the menu colours.
    - Moved the upgrade panel down slightly to not obscure the screen

--- Studio Prototypes/Assets/Scripts/JH_Class_Main.cs
+++ Studio Prototypes/Assets/Scripts/JH_Class_Main.cs
@@ -6,17 +6,24 @@ public class JH_Class_Main : MonoBehaviour
 {
     public GameObject[] upgradeRemove;
     public bool roomUpgraded;
+    private GameObject roomHighlighted;
+    private GameObject upgradeRoomUI;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        upgradeRoomUI = GameObject.Find("Main Camera");
+        roomHighlighted = transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!roomUpgraded && upgradeRoomUI.GetComponent<JH_Upgrade_Room>().selectedRoom == gameObject)
+        {
+            roomHighlighted.SetActive(true);
+        }
+        else roomHighlighted.SetActive(false);
     }
 
     public void UpgradeRoom()
@@ -28,6 +35,7 @@ public class JH_Class_Main : MonoBehaviour
                 upgradeRemove[i].SetActive(false);
             }
 
+            transform.GetChild(1).gameObject.SetActive(false);
             roomUpgraded = true;
         }
     }
