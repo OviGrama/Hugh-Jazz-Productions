6eeab3ff143b6f6ab81e73857a5b8244b33a6bb1
ACraughwell <44976121+ACraughwell@users.noreply.github.com>
2019-03-18 12:51:54 +0000

    Alpha-Update-8
    Student Spawner has been added and works with Ovi's student info script.

---
+++ Studio Prototypes/Assets/Scripts/AC_StudentSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AC_StudentSpawner : MonoBehaviour
+{
+
+    // Hold each of the spawn
+    public GameObject prefab_Student;
+    public GameObject[] go_Students;
+    public int numberOfStudents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnStudents();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnStudents()
+    {
+
+        for (int i = 0; i < numberOfStudents; i++)
+        {
+            go_Students[i] = Instantiate(prefab_Student, new Vector3(50, 0, -30), Quaternion.identity);
+        }
+
+    }
+}
