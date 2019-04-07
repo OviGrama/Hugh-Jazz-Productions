6bcdd5ebe0c4a70e44a661050bd27b4fc038d546
ACraughwell <44976121+ACraughwell@users.noreply.github.com>
2019-04-07 19:16:57 +0100

    Alpha-Update-070419-3
    School Stats finished.
    WeekEnd finished.
    YearEnd Finihsed.

--- Studio Prototypes/Assets/Scripts/AC_WeekEnd.cs
+++ Studio Prototypes/Assets/Scripts/AC_WeekEnd.cs
@@ -5,27 +5,31 @@ using UnityEngine.UI;
 
 public class AC_WeekEnd : MonoBehaviour
 {
-    // Connects to AC_StudentSpawner script, to get info on each of the students info taht is attached to the student objects.
+    // Connects to JH_Student_Manager script, to get info on each of the students info taht is attached to the student objects.
     private JH_Student_Manager studentSpawner;
     //
     private AC_SchoolStatsManager schoolStats;
 
     // Variables to hold the current student that is being checked, stats.
+    private int currentTotalHAP;
+    private int currentTotalALI;
+    // 
     private int currentAverageHAP;
     private int currentAverageALI;
 
 
-    // Start is called before the first frame update
+
+    // Start is called before the first frame update.
     void Start()
     {
-        // So the this script doesnt lose the reference to the AC_StudentSpawner script.
+        // So the this script doesnt lose the reference to the JH_Student_Manager script.
         studentSpawner = GameObject.Find("Game").GetComponent<JH_Student_Manager>();
 
         //
         schoolStats = GameObject.Find("SchoolStatDropDown").GetComponent<AC_SchoolStatsManager>();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame.
     void Update()
     {
 
@@ -33,13 +37,25 @@ public class AC_WeekEnd : MonoBehaviour
 
     public void WeeklyStudentUpdate()
     {
+        currentTotalHAP = 0;
+        currentTotalALI = 0;
+
         for (int i = 0; i < studentSpawner.go_studentList.Length; i++)
         {
+            // Gets total of all student stats.
             if (studentSpawner.go_studentList[i] != null)
             {
-                currentAverageALI = studentSpawner.go_studentList[i].GetComponent<JH_Student_Stats>().happinessLevel;
-                currentAverageHAP = studentSpawner.go_studentList[i].GetComponent<JH_Student_Stats>().alignmentLevel;
+                currentTotalHAP += studentSpawner.go_studentList[i].GetComponent<JH_Student_Stats>().happinessLevel;
+                currentTotalALI += studentSpawner.go_studentList[i].GetComponent<JH_Student_Stats>().alignmentLevel;
             }
         }
+
+        // Finds averages.
+        currentAverageHAP = currentTotalHAP / studentSpawner.go_studentList.Length;
+        currentAverageALI = currentTotalALI / studentSpawner.go_studentList.Length;
+
+        // Updates the school stats dropdown.
+        schoolStats.avgHappy = currentAverageHAP;
+        schoolStats.avgMoral = currentAverageALI;
     }
 }
