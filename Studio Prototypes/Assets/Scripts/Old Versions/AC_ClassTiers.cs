4793e1fecb6eb9c6edc8335b2aced1dd2fa95d53
ACraughwell <44976121+ACraughwell@users.noreply.github.com>
2019-04-24 17:20:16 +0100

    Beta-Update-1-RandomEventsComplete
    - All random event decks now move in and out of the total deck as necessary when correct requirements are met.
    - Random Event Reputation deck change is called from the school stats manager script.
    - Problem with the random events only seraching throught the standard deck has been sorted.
    - Problem with events panel not appearing when the event repeats has been solved.
    - Remaining Random Events just need to be created and added to their rescpected array.
    - Created a folder for old now updated scripts to go into to organise the scripts better.

---
+++ Studio Prototypes/Assets/Scripts/Old Versions/AC_ClassTiers.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AC_ClassTiers : MonoBehaviour
+{
+
+    // These hold the bool variables so that they can be used to check the appropriate value.
+    // What tier is the class.
+    public bool tier1Class;
+    public bool tier2Class;
+    // What type of class is it.
+    public bool iqClass;
+    public bool fitnessClass;
+    public bool superClass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
