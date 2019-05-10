d98b965807f6c469bd0d31bf843f9f8791f0bd97
OviGrama <44976044+OviGrama@users.noreply.github.com>
2019-05-10 22:06:42 +0100

    Rooms and Hallway Triggers

---
+++ Studio Prototypes/Assets/Scripts/AC_AmbientMusic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AC_AmbientMusic : MonoBehaviour
+{
+    // Holds the ambient music.
+    public AudioClip roomAudio;
+
+    public bool inRoom = false;
+
+    // Grabs what will play the audio.
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Starts the game on normal background music.
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.clip = roomAudio;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inRoom = true;
+        audioSource.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inRoom = false;
+            audioSource.Stop();
+    }
+}
